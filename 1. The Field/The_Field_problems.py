23fe7e3aa27275053e1ca00228dc3fb353f7f117
wtbaldoumas <wtbaldoumas@gmail.com>
2016-08-16 18:11:06 -0400

    finished The Field

--- 1. The Field/The_Field_problems.py
+++ 1. The Field/The_Field_problems.py
@@ -19,7 +19,7 @@ def myFilter(L, num):
       >>> myFilter([10,15,20,25],10)
       [15, 25]
     '''
-    pass
+    return [x for x in L if x % num != 0]
 
 
 
@@ -32,7 +32,7 @@ def my_lists(L):
     >>> my_lists([0,3])
     [[], [1, 2, 3]]
     '''
-    pass
+    return [[x + 1 for x in range(y)] for y in L]
 
 
 
@@ -55,7 +55,7 @@ def myFunctionComposition(f, g):
       >>> myFunctionComposition(a,b) == {'x':'twentyfour','y':'twentyfive'}
       True
     '''
-    pass
+    return { x:y for (x,y) in zip([a for a in f.keys()], [b for b in g.values()]) }
 
 
 
@@ -73,8 +73,10 @@ Be sure your procedure works for the empty list.
       >>> mySum([3,5,10])
       18
     '''
-    pass
-
+    current = 0
+    for x in L:
+        current += x
+    return current
 
 
 ## 5: (Problem 1.7.5) Multiplying numbers in a list
@@ -91,8 +93,10 @@ Be sure your procedure works for the empty list.
       >>> myProduct([-3,2,4])
       -24
     '''
-    pass
-
+    current = 1
+    for x in L:
+        current *= x
+    return current
 
 
 ## 6: (Problem 1.7.6) Minimum of a list
@@ -110,7 +114,11 @@ Hint: The value of the Python expression float('infinity') is infinity.
     >>> myMin([0,3,5,-2,-5])
     -5
     '''
-    pass
+    current = L[0]
+    for x in L:
+        if x < current:
+            current = x
+    return current
 
 
 
@@ -128,7 +136,10 @@ Be sure your procedure works for the empty list.
     >>> myConcat(['what','is','up'])
     'whatisup'
     '''
-    pass
+    current = ''
+    for x in L:
+        current += x
+    return current
 
 
 
@@ -146,17 +157,20 @@ Be sure your procedure works for the empty list.
     >>> myUnion([set(),{3,5},{3,5}])
     {3, 5}
     '''
-    pass
+    current = set()
+    for x in L:
+        current = current | x
+    return current
 
 
 
 ## 9: (Problem 1.7.10) Complex Addition Practice
 # Each answer should be a Python expression whose value is a complex number.
 
-complex_addition_a = ...
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = 5 + 3j
+complex_addition_b = 0 + 1j
+complex_addition_c = -1 + .001j
+complex_addition_d = .001 + 9j
 
 
 
@@ -175,12 +189,11 @@ def transform(a, b, L):
     >>> transform(3,2,[1,2,3])
     [5, 8, 11]
     '''
-    pass
+    return [((a * z) + b) for z in L]
 
 
 
 ## 11: (Problem 1.7.13) GF(2) Arithmetic
-GF2_sum_1 = ... # answer with 0 or 1
-GF2_sum_2 = ...
-GF2_sum_3 = ...
-
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
